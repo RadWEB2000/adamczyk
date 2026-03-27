c4fedb31ec301f9952d648a0bea3f0e2674d1885
RadWEB2000 <radoslaw.adamczyk2000@gmail.com>
2026-03-27 17:42:08 +0100

    case studies

--- src/components/views/home-view/CaseStudies.tsx
+++ src/components/views/home-view/CaseStudies.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 function Case(){
     return (
         <Link
-            className="w-full"
+            className="w-fullgi"
             href='#'
         >
             <div
